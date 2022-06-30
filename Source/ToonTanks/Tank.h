d75ccf55eaeea32c2682f9c385e5c9e65a66545b
ann_daniels <anna.danielyan01@gmail.com>
2022-06-30 20:18:55 +0400

    Camera Component and Spring Arm Component attached to Tank

--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -13,5 +13,17 @@ UCLASS()
 class TOONTANKS_API ATank : public ABasePawn
 {
 	GENERATED_BODY()
+
+
+public:
+	ATank();
+
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent* Camera;
 	
 };
